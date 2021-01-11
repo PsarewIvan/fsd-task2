883addce1ed6643d3fae03578fc051bf002fdb0d
Ivan Psarew <psarew.ivan@yandex.ru>
2021-01-11 16:38:49 +0300

    Создает класс View

--- src/layers/view.ts
+++ src/layers/view.ts
@@ -7,28 +7,32 @@ export default class View {
   private el: HTMLElement
   private options: Object
 
-  constructor(element, options) {
+  constructor(element: HTMLElement, settings: Object) {
     this.html = `
-      <span class="my-slider-wrapper">
-        <span class="my-slider">
+      <span class="my-slider__wrapper">
+        <span class="my-slider__model">
           <span class="my-slider__line"></span>
-          <span class="my-slider__min">0</span>
-          <span class="my-slider__max">1</span>
-          <span class="my-slider__value">0</span>
+          <span class="my-slider__min">${settings.min}</span>
+          <span class="my-slider__max">${settings.max}</span>
+          <span class="my-slider__value">${settings.value}</span>
         </span>
         <span class="my-slider__bar"></span>
         <span class="my-slider__handle"></span>
       </span>`;
-    this.init(element, options);
+    this.init(element);
+  }
+
+  public updateElement() {
+
   }
 
   private addElements() {
     this.el.insertAdjacentHTML('afterend', this.html);
   }
 
-  private init(element, options) {
+  private init(element: HTMLElement) {
     this.el = element;
-    this.options = options;
+    this.el.classList.add('my-slider__input');
     this.addElements();
   }
 };
