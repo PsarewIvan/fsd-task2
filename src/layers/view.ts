4a9152b1c32a0123be6f2572233996276815538b
Ivan Psarew <psarew.ivan@yandex.ru>
2021-01-12 10:06:19 +0300

    Создает в представлении элементы слайдера и реализует метод его отрисовки

--- src/layers/view.ts
+++ src/layers/view.ts
@@ -4,35 +4,50 @@
 
 export default class View {
   private html: string
-  private el: HTMLElement
-  private options: Object
+  private inputElement: HTMLElement
+  private sliderElement: HTMLElement
+  private barElement: HTMLElement
+  private pinElement: HTMLElement
+  private minElement: HTMLElement
+  private maxElement: HTMLElement
+  private valueElement: HTMLElement
 
   constructor(element: HTMLElement, settings: Object) {
     this.html = `
       <span class="my-slider__wrapper">
         <span class="my-slider__model">
           <span class="my-slider__line"></span>
-          <span class="my-slider__min">${settings.min}</span>
-          <span class="my-slider__max">${settings.max}</span>
-          <span class="my-slider__value">${settings.value}</span>
+          <span class="my-slider__min">0</span>
+          <span class="my-slider__max">1</span>
+          <span class="my-slider__value">0</span>
         </span>
         <span class="my-slider__bar"></span>
         <span class="my-slider__handle"></span>
       </span>`;
-    this.init(element);
-  }
-
-  public updateElement() {
-
+    this.init(element, settings);
   }
 
   private addElements() {
-    this.el.insertAdjacentHTML('afterend', this.html);
+    this.inputElement.insertAdjacentHTML('afterend', this.html);
   }
 
-  private init(element: HTMLElement) {
-    this.el = element;
-    this.el.classList.add('my-slider__input');
+  public drawSlider(settings: Object) {
+    this.minElement.innerHTML = settings.min;
+    this.maxElement.innerHTML = settings.max;
+    this.valueElement.innerHTML = settings.value;
+  }
+
+  private init(element: HTMLElement, settings: Object) {
+    this.inputElement = element;
+    this.inputElement.classList.add('my-slider__input');
     this.addElements();
+    this.sliderElement = this.inputElement.parentElement.querySelector('.my-slider__wrapper');
+    this.barElement = this.sliderElement.querySelector('.my-slider__bar');
+    this.pinElement = this.sliderElement.querySelector('.my-slider__handle');
+    this.minElement = this.sliderElement.querySelector('.my-slider__min');
+    this.maxElement = this.sliderElement.querySelector('.my-slider__max');
+    this.valueElement = this.sliderElement.querySelector('.my-slider__value');
+
+    this.drawSlider(settings);
   }
 };
