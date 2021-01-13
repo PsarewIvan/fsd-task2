bef63fdc794742f0e1d110604388f6b82ce5b0ec
Ivan Psarew <psarew.ivan@yandex.ru>
2021-01-13 12:32:10 +0300

    Добавляет описание переменных в презентер

--- src/layers/presenter.ts
+++ src/layers/presenter.ts
@@ -3,18 +3,19 @@
 // реагирует на сообщения об обновлении модели и обновляет отображение
 import Model from './model';
 import View from './view';
+import { IUserSettings } from './interfaces';
 
 export default class SliderPresenter {
   private model: Model;
   private view: View;
   private element: HTMLElement
 
-  constructor(element: JQuery, options: Object) {
+  constructor(element: JQuery, options: IUserSettings) {
     this.element = element[0];
     this.init(options);
   }
 
-  private init(options: Object) {
+  private init(options: IUserSettings) {
     this.model = new Model(options);
     this.view = new View(this.element, this.model.getSettings());
     this.model.modelChangedSubject.subscribe( () => {
