3300854ac525c807116785a5a883ed7491c85760
Ivan Psarew <psarew.ivan@yandex.ru>
2021-01-16 00:11:20 +0300

    Комментирует анализатор кода

--- webpack.config.js
+++ webpack.config.js
@@ -77,9 +77,9 @@ const plugins = () => {
     }),
   ];
 
-  if (isProd) {
-    base.push( new BundleAnalyzerPlugin() )
-  }
+  // if (isProd) {
+  //   base.push( new BundleAnalyzerPlugin() )
+  // }
 
   if (isDev) {
     base.push( new webpack.HotModuleReplacementPlugin() );
