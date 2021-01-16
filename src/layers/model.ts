476a5c1572eb8faafb0f35dfa610fb769fdf7a9a
Ivan Psarew <psarew.ivan@yandex.ru>
2021-01-16 16:31:32 +0300

    Убирает обсервер из модели

--- src/layers/model.ts
+++ src/layers/model.ts
@@ -1,12 +1,10 @@
 // Слой управления данными, который содержит бизнес-логику
 
-import MakeObservableSubject from './makeObservableSubject';
 import { IModelSettings, IUserSettings } from './interfaces';
 
 export default class Model {
   readonly defaults: IModelSettings;
   private settings: IModelSettings;
-  public modelChangedSubject: MakeObservableSubject;
 
   constructor(options: IUserSettings) {
     this.defaults = {
@@ -14,13 +12,11 @@ export default class Model {
       max: 100,
       value: 50
     };
-    this.modelChangedSubject = new MakeObservableSubject();
     this.setSettings(options);
   }
 
   public setSettings(newSettings: IUserSettings) {
     this.settings = {...this.defaults, ...newSettings};
-    this.modelChangedSubject.notify();
   }
 
   public getSettings(): IModelSettings {
