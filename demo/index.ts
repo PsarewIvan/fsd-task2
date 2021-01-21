66d104784f8f60e256730f56509f04044a5a8a13
Ivan Psarew <psarew.ivan@yandex.ru>
2021-01-21 18:42:35 +0300

    Инициализирует слайдеры с новыми параметрами

--- demo/index.ts
+++ demo/index.ts
@@ -6,18 +6,18 @@ $('.free-slider').freeSlider({
   min: 0,
   max: 10,
   value: 2,
-  step: 0.1
-  // onChange: function(value) {
-  //   console.log('OnChange Value: ', value);
-  // },
-  // onFinish: function (value) {
-  //   console.log('OnFinish Value: ', value);
-  // }
+  step: 0.1,
+  onChange: function(value) {
+    console.log('OnChange Value: ', value);
+  },
+  onFinish: function (value) {
+    console.log('OnFinish Value: ', value);
+  }
 });
 
 $('.free-slider2').freeSlider({
   min: 10,
-  max: 303,
-  value: 100.35,
-  step: 10
+  max: 30,
+  value: 20,
+  step: 1.25
 });
