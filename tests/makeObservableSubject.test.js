2eb6aeb3dc5c0eb1972cfbdda16a2b656d42134c
Ivan Psarew <psarew.ivan@yandex.ru>
2021-01-27 10:46:25 +0300

    Создает тесты для обсервера

--- tests/makeObservableSubject.test.js
+++ tests/makeObservableSubject.test.js
@@ -1,13 +1,49 @@
 import MakeObservableSubject from '../src/components/makeObservableSubject';
 
-const Observer = new MakeObservableSubject();
+let Observer;
+let mockData;
 const someFunction = (someData) => {
-  return someData;
+  mockData = someData;
 };
 
+beforeEach( () => {
+  Observer = new MakeObservableSubject();
+  mockData = 'empty';
+});
+
 describe ('Make observable subject', () => {
-  test('subscribe callback', () => {
-    const result = 'a';
-    expect(someFunction('a')).toBe(result);
+  test('should return "This is data"', () => {
+    Observer.subscribe( (data) => {
+      someFunction(data)});
+    Observer.notify('This is data');
+
+    expect(mockData).toBe('This is data');
+  });
+
+  test('should return "345"', () => {
+    Observer.subscribe( (data) => {
+      someFunction(data)});
+    Observer.notify(345);
+
+    expect(mockData).toBe(345);
+  });
+
+  test('should return "null"', () => {
+    Observer.subscribe( (data) => {
+      someFunction(data)});
+    Observer.notify(null);
+
+    expect(mockData).toBeNull();
+  });
+
+  test('should unsubscribe', () => {
+    const testFunction = (data) => {
+      someFunction(data);
+    }
+    Observer.subscribe(testFunction);
+    Observer.unsubscribe(testFunction)
+    Observer.notify(null);
+
+    expect(mockData).toBe('empty');
   });
 });
