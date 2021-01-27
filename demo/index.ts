d91d574852bc4ba9e631f1eecfbb51c68f3ce358
Ivan Psarew <psarew.ivan@yandex.ru>
2021-01-27 23:55:24 +0300

    Инициализирует 2 вертикальных слайдера

--- demo/index.ts
+++ demo/index.ts
@@ -2,12 +2,12 @@ import './styles.scss';
 import $ from 'jquery';
 import '../src/';
 
-const slider = $('.free-slider');
+const slider = $('.slider');
 
 slider.freeSlider({
-  min: 0,
-  max: 10,
-  value: 2,
+  min: -100,
+  max: -80,
+  value: -90,
   step: 0.1,
   // onChange: function(value) {
   //   console.log('OnChange Value: ', value);
@@ -19,11 +19,29 @@ slider.freeSlider({
 
 // console.log(slider.freeSlider('getValue'));
 
-$('.free-slider2').freeSlider({
+$('.slider--range').freeSlider({
   type: 'range',
-  min: 10,
+  min: -100,
   max: 300,
   from: 30,
   to: 100,
   step: 2
+});
+
+$('.slider--vertical').freeSlider({
+  orientation: 'vertical',
+  min: 0,
+  max: 100,
+  value: 30,
+  step: 1
+});
+
+$('.slider--vertical--range').freeSlider({
+  orientation: 'vertical',
+  type: 'range',
+  min: -100,
+  max: 100,
+  from: 0,
+  to: 50,
+  step: 5
 });
