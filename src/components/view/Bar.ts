ad2f153f8ef28ed75b5691d4c34c8ed6c4574edf
Ivan Psarew <psarew.ivan@yandex.ru>
2021-02-05 12:34:13 +0300

    Убирате лишний код в Bar

--- src/components/view/Bar.ts
+++ src/components/view/Bar.ts
@@ -27,22 +27,4 @@ export default class TrackView extends SliderElement {
       }
     }
   }
-
-  // public moveBar(values: Array<number>) {
-  //   if (this.orientation === 'vertical') {
-  //     if (this.type === 'range') {
-  //       this.root.style.top = `${values[0]}px`;
-  //       this.root.style.height = `${values[1] - values[0]}px`;
-  //     } else {
-  //       this.root.style.height = `${values[0]}px`;
-  //     }
-  //   } else if (this.orientation === 'horizontal') {
-  //     if (this.type === 'range') {
-  //       this.root.style.left = `${values[0]}px`;
-  //       this.root.style.width = `${values[1] - values[0]}px`;
-  //     } else {
-  //       this.root.style.width = `${values[0]}px`;
-  //     }
-  //   }
-  // }
 }
