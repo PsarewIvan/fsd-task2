10172dd4526d800997bd52dc40e5be78fc8de313
Ivan Psarew <psarew.ivan@yandex.ru>
2021-02-18 20:38:25 +0300

    Добавляет пробные тесты класса Track

--- tests/track.test.js
+++ tests/track.test.js
@@ -1,10 +1,15 @@
-import '@testing-library/jest-dom/extend-expect';
-import { getAllByText } from '@testing-library/dom';
 import Track from '../src/components/view/Track';
+import { screen, getAllByText } from '@testing-library/dom';
+import '@testing-library/jest-dom/extend-expect';
 
 const state = {
   type: 'single',
   orientation: 'horizontal',
+};
+
+const stateVertical = {
+  type: 'single',
+  orientation: 'vertical',
 };
 
 it('Render Track', () => {
@@ -16,3 +21,8 @@ it('Accepts `className`', () => {
   const track = new Track(document.body, state);
   expect(track.root).toHaveClass('free-slider__track');
 });
+
+// it('Return sizes for horizontal', () => {
+//   const track = new Track(document.body, state);
+//   expect(track.getTrackSize()).toBe(100);
+// });
