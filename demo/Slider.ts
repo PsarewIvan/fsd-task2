882bb295e463c3a594d81424ee839a68ea89ca64
Ivan Psarew <psarew.ivan@yandex.ru>
2021-02-18 15:25:26 +0300

    Создает в классе слайдера на демо странице метод получения состояния

--- demo/Slider.ts
+++ demo/Slider.ts
@@ -59,4 +59,8 @@ export default class Slider {
   public setValues(values: number[]): void {
     this.slider.freeSlider('setValue', values);
   }
+
+  public getState() {
+    return this.slider.freeSlider('getState');
+  }
 }
