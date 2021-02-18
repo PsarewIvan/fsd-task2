6c38040372d0d27fbe97dddb72c1332eadc97264
Ivan Psarew <psarew.ivan@yandex.ru>
2021-02-18 23:35:43 +0300

    Создает StepInput для демо-страницы

---
+++ demo/scripts/Slider/components/StepInput.ts
@@ -0,0 +1,19 @@
+import { Settings } from '../types';
+
+export default class StepInput {
+  private root: HTMLInputElement;
+
+  constructor(slider: JQuery, state: Settings) {
+    this.root = document.createElement('input');
+    this.root.classList.add('slider__input-step');
+    this.root.type = 'number';
+    this.root.min = `0`;
+    this.root.max = (state.max - state.min).toString();
+    this.root.value = state.step.toString();
+    const label = document.createElement('label');
+    label.classList.add('slider__label', 'slider__label--step');
+    label.innerHTML = `Step: `;
+    label.append(this.root);
+    slider.after(label);
+  }
+}
