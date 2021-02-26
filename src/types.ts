ab288e39731f15b44034eda52f303860cb3b8ee1
Ivan Psarew <psarew.ivan@yandex.ru>
2021-02-26 23:15:09 +0300

    Правит типы

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import SliderElement from './components/view/SliderElement';
+
 export interface Settings {
   min: number;
   max: number;
@@ -30,15 +32,15 @@ export interface Methods {
 
 export interface RequiredThumb {
   index: number;
-  root: HTMLElement;
+  root: SliderElement;
 }
 
 export type ThumbType = 'single' | 'from' | 'to';
 export type SliderType = 'single' | 'range';
 export type SliderOrientation = 'vertical' | 'horizontal';
 export type DirectionType = 'left' | 'top';
-export type SizeType = 'offsetWidth' | 'offsetHeight';
-export type SizeTypeCss = 'width' | 'height';
+export type OffsetSizeType = 'offsetWidth' | 'offsetHeight';
+export type SizeType = 'width' | 'height';
 export type CoordType = 'clientX' | 'clientY';
 
 declare global {
