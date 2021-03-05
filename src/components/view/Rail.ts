9d6cbe8b88790434d4b2f4df0109ccf542dac1a9
Ivan Psarew <psarew.ivan@yandex.ru>
2021-03-05 13:33:21 +0300

    Убирает неиспользуемый код

--- src/components/view/Rail.ts
+++ src/components/view/Rail.ts
@@ -2,10 +2,7 @@ import SliderElement from './SliderElement';
 import { Settings } from '../../types';
 
 export default class Rail extends SliderElement {
-  readonly state: Settings;
-
   constructor(rootNode: HTMLElement, state: Settings) {
     super(rootNode, ['free-slider__rail'], state.orientation);
-    this.state = state;
   }
 }
--- src/components/view/Track.ts
+++ src/components/view/Track.ts
@@ -2,18 +2,9 @@ import SliderElement from './SliderElement';
 import { Settings } from '../../types';
 
 export default class TrackView extends SliderElement {
-  readonly state: Settings;
-
   constructor(rootNode: HTMLElement, state: Settings) {
     super(rootNode, ['free-slider__track'], state.orientation);
-    this.state = state;
   }
-
-  // Возвращает расстояние от трека до края экрана
-  public getDistanceToScreen(): number {
-    return this.root.getBoundingClientRect()[this.directionType];
-  }
-
   // Слушатель для обработки клика по треку
   public clickEvent(handler: Function): void {
     this.root.addEventListener('pointerdown', (evt: PointerEvent) => {
